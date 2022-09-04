a369aaf21994e4ec16298b458d31b96abfedcd72
Даниил <112555991+Dan4ello2077@users.noreply.github.com>
2022-09-04 19:33:01 +0300

    V 2.0

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from pygame import *
 from random import randrange as rnd
 
-window = display.set_mode((700, 500))
-backgraund = transform.scale(image.load('лес.jpg'), (700, 500))
+window = display.set_mode((800, 601))
+backgraund = transform.scale(image.load('лес.jpg'), (800, 700))
 fps = 60
 clock = time.Clock()
 run = True
 class GameSprite(sprite.Sprite):
-    def __init__(self, plaer_image, plare_speed, plaer_x, plaer_y, wedhe = 85, heid = 65):
+    def __init__(self, plaer_image, plare_speed, plaer_x, plaer_y, wedhe = 65, heid = 150):
         super(). __init__()
         self.step = 0
         self.image = transform.scale(image.load(plaer_image),(wedhe,heid))
@@ -21,21 +21,29 @@ class GameSprite(sprite.Sprite):
 class Plaer(GameSprite):
     def update_l(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > -35:
-            self.rect.y -= 9
-        if keys_pressed[K_DOWN] and self.rect.y < 445:
-            self.rect.y += 9
+        if keys_pressed[K_UP] and self.rect.y > 0:
+            self.rect.y -= 4
+        if keys_pressed[K_DOWN] and self.rect.y < 450:
+            self.rect.y += 4
     def update_r(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > -35:
-            self.rect.y -= 9
-        if keys_pressed[K_DOWN] and self.rect.y < 445:
-            self.rect.y += 9
+        if keys_pressed[K_w] and self.rect.y > -10:
+            self.rect.y -= 4
+        if keys_pressed[K_s] and self.rect.y < 465:
+            self.rect.y += 4
+
+plaer_l = Plaer('плотформа-PhotoRoom.png', 50, 700, 205)
+plaer_r = Plaer('plotform (1).png', 0, 20, 205)
+sphere = Plaer ('сфера.png', 0, 390, 265, 45, 45)
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
     window.blit(backgraund, (0, 0))
-
+    plaer_r.update_r()
+    plaer_l.update_l()
+    plaer_l.reset()
+    plaer_r.reset()
+    sphere.reset()
     display.update()
     clock.tick(fps)
